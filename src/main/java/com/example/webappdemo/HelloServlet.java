cfa8094fbc97a63fe51e496c61cdc91d2244064f
saxni03 <saxni03@ca.com>
2018-07-26 00:11:25 +0530

    Added tomcat plugin

---
+++ src/main/java/com/example/webappdemo/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.example.webappdemo;
+
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException
+
+    {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
